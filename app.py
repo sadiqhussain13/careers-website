12ae9aac5b0ce8624c88e9a733a2f0c99836ad4d
sadiqhussain13 <sadiqhussain13@gmail.com>
2024-10-09 21:04:45 +0530

    Created dynamic pages for each job role.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from localdb import load_jobs_from_db
+from localdb import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 @app.route("/")
@@ -11,7 +11,14 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
-  return jsonify([job for job in jobs])
+  return jsonify([jobs])
+
+@app.route("/job/<id>")
+def get_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return 'Not found', 404
+  return render_template('jobpage.html', job=job)
 
 print(__name__)
 if (__name__) == "__main__":
--- localdb.py
+++ localdb.py
@@ -6,30 +6,59 @@ dotenv.load_dotenv()
 
 # Load jobs from database
 def load_jobs_from_db():
-  conn = psycopg2.connect(
-    host=os.environ.get('DATABASE_HOST'),
-    database=os.environ.get('DATABASE_NAME'),
-    user=os.environ.get('DATABASE_USERNAME'),
-    password=os.environ.get('DATABASE_PASSWORD')
-  )
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM jobs")
-  results = cur.fetchall()
-  jobs = []
-  for row in results:
-    
+  try:
+    conn = psycopg2.connect(
+      host=os.environ.get('DATABASE_HOST'),
+      database=os.environ.get('DATABASE_NAME'),
+      user=os.environ.get('DATABASE_USERNAME'),
+      password=os.environ.get('DATABASE_PASSWORD')
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM jobs")
+    results = cur.fetchall()
+    jobs = []
+    for row in results:
+      job = {
+        'id': row[0],
+        'title': row[1],
+        'location': row[2],
+        'salary': row[3],
+        'currency': row[4],
+        'responsibilities': row[5],
+        'requirements': row[6]
+      }
+      jobs.append(job)
+    conn.close()
+    return jobs
+  except psycopg2.Error as e:
+    print(f"Error loading jobs from database: {e}")
+
+def load_job_from_db(id):
+  try:
+    conn = psycopg2.connect(
+      host=os.environ.get('DATABASE_HOST'),
+      database=os.environ.get('DATABASE_NAME'),
+      user=os.environ.get('DATABASE_USERNAME'),
+      password=os.environ.get('DATABASE_PASSWORD')
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM jobs WHERE id = %s", (id,))
+    results = cur.fetchall()
+    if not results:
+      return None
     job = {
-      'id': row[0],
-      'title': row[1],
-      'location': row[2],
-      'salary': row[3],
-      'currency': row[4],
-      'responsibilities': row[5],
-      'requirements': row[6]
+      'id': results[0][0],
+      'title': results[0][1],
+      'location': results[0][2],
+      'salary': results[0][3],
+      'currency': results[0][4],
+      'responsibilities': results[0][5],
+      'requirements': results[0][6]
     }
-    jobs.append(job)
-  conn.close()
-  return jobs
+    conn.close()
+    return job
+  except psycopg2.Error as e:
+    print(f"Error loading job from database: {e}")
 
 # # To check the type
 # print("type of results: ", type(load_jobs_from_db()))
